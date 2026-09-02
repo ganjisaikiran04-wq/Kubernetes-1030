92aece87402f73cbfda88a423a5fabbb2d452818
ganjisaikiran04-wq <ganjisaikiran04@gmail.com>
2026-09-02 14:30:29 +0530

    Update test.sh

--- Day-5-HPA&cluster-autoscaler/test.sh
+++ Day-5-HPA&cluster-autoscaler/test.sh
@@ -1,6 +1,6 @@
 for j in {1..10}; do  
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a8ec4d33f89bf48abae83ded175662d8-1084698997.us-east-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" http://a58a45b202bcf45bfb1f83f07dfc8008-1202968466.us-east-1.elb.amazonaws.com/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
